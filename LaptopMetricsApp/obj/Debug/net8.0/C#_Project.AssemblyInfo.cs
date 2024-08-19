fc295c6a5695f97d659f48f23897bfe4d0b7f348
Hamza Harti <hamza.harti94@gmail.com>
2024-08-19 14:34:42 +0200

    C# Update

--- LaptopMetricsApp/obj/Debug/net8.0/C#_Project.AssemblyInfo.cs
+++ LaptopMetricsApp/obj/Debug/net8.0/C#_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e354dd7eae36cccc10b254be77be9f16c637d886")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
